2a6950a5ccca34efdcee5a1d00b08dadf7e8c15c
horsemaker <yash_ghodekar@yahoo.in>
2020-12-28 16:16:43 +0530

    Pages Setup.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 
 export default function App() {
   return(
     <Router>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <SignIn />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <SignUp />
+      </Route>
       <Route exact path={ROUTES.HOME}>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Home />
       </Route>
     </Router>
   );
