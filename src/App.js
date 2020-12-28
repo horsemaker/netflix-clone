8c64a449960ee34b18bf365f794a2076ca8302d0
horsemaker <yash_ghodekar@yahoo.in>
2020-12-28 15:48:37 +0530

    Setup Routes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
-  return <>
-    <JumbotronContainer />
-    <FaqsContainer />
-    <FooterContainer />
-  </>
+  return(
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </Route>
+    </Router>
+  );
 }
