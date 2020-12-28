bf9e9df87938339c8918d6c7a315bae0e5733d52
horsemaker <yash_ghodekar@yahoo.in>
2020-12-28 13:18:16 +0530

    Footer Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
+import { FooterContainer } from './containers/footer';
 
 export default function App() {
-  return <JumbotronContainer />
+  return <>
+    <JumbotronContainer />
+    <FooterContainer />
+  </>
 }
